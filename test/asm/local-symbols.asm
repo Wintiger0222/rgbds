45a7de67113f97c5eb6c3eb950435e3e385ce9ea
ISSOtm <eldredhabert0@gmail.com>
2019-10-09 14:32:52 +0200

    Add tests

---
+++ test/asm/local-symbols.asm
@@ -0,0 +1,11 @@
+SECTION "Test", ROM0[0]
+
+GlobalScope: ; Define a global scope for locals to not error out on that
+
+.label:
+.equ EQU 0
+.equs EQUS "Hello tester"
+.set SET 42
+.macro: MACRO
+    ;
+ENDM
